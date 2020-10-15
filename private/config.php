df946d01cdd0df77861086a68911fc384551dc76
suryavip <suryabejibun@gmail.com>
2020-10-15 19:18:53 +0700

    add wait between request config

--- private/config.php
+++ private/config.php
@@ -4,4 +4,5 @@ class Config
 {
 	const baseCurrency = 'EUR';
 	const numberOfRequests = 7;  # 2 pairs per requests. 6 requests means 12 pairs
+	const waitBetweenRequest = 3;  # wait 3 seconds before doing next request
 }
